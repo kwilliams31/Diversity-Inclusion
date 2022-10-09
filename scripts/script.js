0665c034a1dfa74152b7afe69c1fb570219e6b6f
Kristina <111464160+kwilliams31@users.noreply.github.com>
2022-10-09 13:27:54 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+/*
+$(document).ready(function() {  
+  $("#sendName").click(function() {         
+  $("#greetingOutput").text("Hi " + $("#fname").val() + "! Hope you are doing well. Sending good vibes!");
+      });
+      });
+      
+      */
